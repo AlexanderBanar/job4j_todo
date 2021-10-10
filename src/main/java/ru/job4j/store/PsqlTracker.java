d4960c234b1393b942622343eb288e803687aae6
AlexanderBanar <s595659@mail.ru>
2021-10-10 10:22:24 +0300

    3. Лямбды и шаблон wrapper. [#49295]

--- src/main/java/ru/job4j/store/PsqlTracker.java
+++ src/main/java/ru/job4j/store/PsqlTracker.java
@@ -11,6 +11,7 @@ import ru.job4j.models.Item;
 
 import java.sql.Timestamp;
 import java.util.List;
+import java.util.function.Function;
 
 public class PsqlTracker implements AutoCloseable {
     private final StandardServiceRegistry registry;
@@ -31,45 +32,57 @@ public class PsqlTracker implements AutoCloseable {
         return Lazy.INST;
     }
 
-    public List<Item> findAll() {
-        Session session = sf.openSession();
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = sf.openSession();
         session.beginTransaction();
-        List<Item> result = session.createQuery("from ru.job4j.models.Item").list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        try {
+            T rsl = command.apply(session);
+            session.getTransaction().commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
+    }
+
+    public List<Item> findAll() {
+        return this.tx(
+                session -> session.createQuery("from ru.job4j.models.Item").list()
+        );
     }
 
     public List<Item> findAllOpen() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Query<Item> query = session.createQuery(
-                "from ru.job4j.models.Item where done = :parameter");
-        query.setParameter("parameter", false);
-        List<Item> result = query.list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return this.tx(
+                session -> {
+                    Query<Item> query = session.createQuery(
+                            "from ru.job4j.models.Item where done = :parameter");
+                    query.setParameter("parameter", false);
+                    return query.list();
+                }
+        );
     }
 
-    public void closeTask(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Query query = session.createQuery(
-                "update ru.job4j.models.Item set done = true where id = :parameterId");
-        query.setParameter("parameterId", id);
-        query.executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+    public List<Item> closeTask(int id) {
+        return this.tx(
+                session -> {
+                    Query<Item> query = session.createQuery(
+                            "update ru.job4j.models.Item set done = true where id = :parameterId");
+                    query.setParameter("parameterId", id);
+                    return query.list();
+                }
+        );
     }
 
-    public void save(String description) {
-        Item item = new Item(description, new Timestamp(System.currentTimeMillis()), false);
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.save(item);
-        session.getTransaction().commit();
-        session.close();
+    public List<Item> save(String description) {
+        return this.tx(
+                session -> {
+                    Item item = new Item(description, new Timestamp(System.currentTimeMillis()), false);
+                    session.save(item);
+                    return List.of(item);
+                }
+        );
     }
 
     @Override
